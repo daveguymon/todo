077df666372d46f1f09c5524d1a4e07635db7831
Dave Guymon <davidwguymon@gmail.com>
2020-03-14 18:04:51 -0600

    Config with scss and for deploy to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2020_03_14_220316) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todo_items", force: :cascade do |t|
     t.string "content"
-    t.integer "todo_list_id", null: false
+    t.bigint "todo_list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "completed_at"
